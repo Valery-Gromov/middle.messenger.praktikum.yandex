0d28483277da391ced485f28f2cefc2bb7653aaf
Valerii Gromov <kar.tell@yandex.ru>
2024-03-15 17:50:50 +0200

    Added empty lines in files

--- src/pages/Login/index.ts
+++ src/pages/Login/index.ts
@@ -6,10 +6,9 @@ import astronautImage from '../../assets/images/astronaut.png';
 import logoImage from '../../assets/images/logo.svg';
 
 Handlebars.registerHelper('astronautImage', () => {
-    return astronautImage;
-  });
-  
-  Handlebars.registerHelper('logoImage', () => {
-      return logoImage;
-    });
-    
+  return astronautImage;
+});
+
+Handlebars.registerHelper('logoImage', () => {
+  return logoImage;
+});
